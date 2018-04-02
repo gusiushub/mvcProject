7398175fc92413dc1219b363fb6c5190c7b57ac4
nikita <zolowkin.nikita@yandex.ru>
2018-04-02 22:32:17 +0300

    added a class to work with the database

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,12 +2,20 @@
 
 namespace app\controllers;
 
+
 use app\core\Controller;
+use app\lib\Db;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
+        $db = new Db;
+        $params = [
+            'id' => 2,
+        ];
+        $data = $db->column('SELECT name FROM users WHERE id = :id',$params);
+        debug($data);
         $vars = [
             'name' => 'Vasia',
             'age' => '10',
--- app/lib/Db.php
+++ app/lib/Db.php
@@ -2,9 +2,39 @@
 
 namespace app\lib;
 
-class Db {
+use PDO;
+
+class Db
+{
+    protected $db;
+
     function __construct()
     {
-        echo 'это класс Db';
+        $config = require 'app/config/db.php';
+        $this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['db_name'].'', $config['username'], $config['password']);
+    }
+
+    public function query($sql, $params = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        if(!empty($params)){
+            foreach ($params as $key => $val){
+                $stmt->bindValue(':'.$key, $val);
+            }
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function row($sql, $params = [])
+    {
+        $result = $this->query($sql, $params);
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function column($sql, $params = [])
+    {
+        $result = $this->query($sql, $params);
+        return $result->fetchColumn();
     }
 }
